dc02e1f0390132e697e45bb9ce0af33338b1b4f9
LUSEDOU <dolorier_uribe@hotmail.com>
2022-08-17 19:16:54 -0500

    feat(1.4): join all functions

--- homework1.4/problem_set.py
+++ homework1.4/problem_set.py
@@ -48,32 +48,51 @@ def localize(colors, measurements, motions, sensor_right, p_move):
     pInit = 1.0 / float(len(colors)) / float(len(colors[0]))
     p = [[pInit for row in range(len(colors[0]))]
          for col in range(len(colors))]
-    p[1][2] = 1
-    show(p)
 
     # >>> Insert your code here <<<
     # For every event
     rows = len(p)
     columns = len(p[0])
 
-    def move(p, move):
+    def event(p, nMove, nSense):
         q = []
-        for i in range(len(p)):
+        for i in range(rows):
             q_row = []
-            for j in range(len(p[0])):
-                pMove = p_move * p[((i-move[0]) % rows)][(j-move[1]) % columns]
-                pMove = pMove + ((1-p_move)*p[i][j])
-                q_row.append(pMove)
+            for j in range(columns):
+                pMove = move(p, i, j, nMove)
+                pSense = sense(colors, i, j, nSense)
+                q_row.append(pMove * pSense)
             q.append(q_row)
         return q
 
-    for nEvent in range(len(motions)):
-        p = move(p, motions[nEvent])
-    return p
+    def move(p, i, j, move):
+        # Hit
+        pMove = p_move * p[((i-move[0]) % rows)][(j-move[1]) % columns]
+        # Miss
+        pMove = pMove + ((1-p_move)*p[i][j])
+        return pMove
 
-# [0, 0, 0]
-# [0, 0, 0]
-# [0, 0, 0]
+    def sense(colors, i, j, sense):
+        hit = (sense == colors[i][j])
+        # Hit
+        pSense = hit * sensor_right
+        # Miss
+        pSense = pSense + ((1 - hit) * (1-sensor_right))
+        return pSense
+
+    def normalizer(p):
+        s = 0
+        for i in range(rows):
+            s += sum(p[i])
+
+        for i in range(rows):
+            for j in range(columns):
+                p[i][j] = p[i][j] / s
+
+    for nEvent in range(len(motions)):
+        p = event(p, motions[nEvent], measurements[nEvent])
+        normalizer(p)
+    return p
 
 
 def show(p):
@@ -81,12 +100,14 @@ def show(p):
         '[' + ','.join(map(lambda x: '{0:.5f}'.format(x), r)) + ']' for r in p]
     print('[' + ',\n '.join(rows) + ']')
 
+
 #############################################################
-# For the following test case, y988, 0.00935],
+# For the following test case, your output should be
+# [[0.01105, 0.02464, 0.06799, 0.04472, 0.02465],
+#  [0.00715, 0.01017, 0.08696, 0.07988, 0.00935],
 #  [0.00739, 0.00894, 0.11272, 0.35350, 0.04065],
 #  [0.00910, 0.00715, 0.01434, 0.04313, 0.03642]]
 # (within a tolerance of +/- 0.001 for each entry)
-
 
 colors = [['R', 'G', 'G', 'R', 'R'],
           ['R', 'R', 'G', 'R', 'R'],
@@ -95,6 +116,4 @@ colors = [['R', 'G', 'G', 'R', 'R'],
 measurements = ['G', 'G', 'G', 'G', 'G']
 motions = [[0, 0], [0, 1], [1, 0], [1, 0], [0, 1]]
 p = localize(colors, measurements, motions, sensor_right=0.7, p_move=0.8)
-show(p)  # displays your answer our output should be
-# [[0.01105, 0.02464, 0.06799, 0.04472, 0.02465],
-#  [0.00715, 0.01017, 0.08696, 0.07
+show(p)  # displays your answer
